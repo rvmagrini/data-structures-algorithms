c5192366334bf853412f4f52f592f41d9bd9b703
Renato Magrini <renatomagrini@gmail.com>
2021-12-11 10:28:26 +0100

    [AG-2.2] Checking if a String is a valid palindrome

--- src/main/java/com/rvmagrini/algorithms/string/Palindrome.java
+++ src/main/java/com/rvmagrini/algorithms/string/Palindrome.java
@@ -4,23 +4,21 @@ public class Palindrome {
 
     public static void main(String[] args) {
 
-        System.out.println("Is '1mA... daM1' Palindrome: " + isPalindrome("1mA... daM1"));
-        System.out.println("Is 'A man, a plan, a canal: Panama' Palindrome: " + isPalindrome("A man, a plan, a canal: Panama"));
-        System.out.println("Is 'race a car' Palindrome: " + isPalindrome("race a car"));
+        System.out.println("Is '1mA... daM1' a valid Palindrome? -> " + isPalindrome("1mA... daM1"));
+        System.out.println("Is 'A man, a plan, a canal: Panama' a valid Palindrome? -> " + isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println("Is 'race a car' a valid Palindrome? -> " + isPalindrome("race a car"));
 
     }
 
     public static boolean isPalindrome(String string) {
-        String alphaNumStr = "";
-
+        StringBuilder alphaNumStrBuilder = new StringBuilder();
         for (char c : string.toCharArray()) {
             if (Character.isDigit(c) || Character.isLetter(c)) {
-                alphaNumStr += c;
+                alphaNumStrBuilder.append(c);
             }
         }
 
-        alphaNumStr = alphaNumStr.toLowerCase();
-
+        String alphaNumStr = alphaNumStrBuilder.toString().toLowerCase();
         int startPointer = 0;
         int endPointer = alphaNumStr.length()-1;
         while (startPointer < endPointer) {
